f71fa8f54b5cffeedde064bf1448ba25acd1af7a
Harshini Venkataraman <42005532+HarshiniVenkat@users.noreply.github.com>
2023-09-17 05:33:16 -0700

    upgrades (#2)
    Co-authored-by: harshven <harshven@cisco.com>

--- app/api/api_v1/endpoints/actions.py
+++ app/api/api_v1/endpoints/actions.py
@@ -9,7 +9,7 @@ from starlette import status
 from starlette.responses import JSONResponse
 
 from app.core.config import Settings, logger
-from app.models.collection import CollectionModel, CollectionInsert
+from app.models.collection import *
 from app.models.document import Document, UpdateDocument
 
 from app.utils import exception, logging, helper
@@ -19,6 +19,18 @@ router = APIRouter()
 settings = Settings()
 
 client = MongoClient(settings.uri)
+
+@router.post("/find", response_model=Any,
+             responses={200: {"model": Any}, 400: {"model": exception.HTTPError}})
+def find(body: CollectionFind = Body(...)):
+    try:
+        document_metadata = jsonable_encoder(body)
+        db = client[document_metadata['database']]
+        single_doc = db[document_metadata['collection']].find()
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(json_util.dumps(single_doc)))
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
 
 @router.post("/findOne", response_model=Document,
@@ -46,6 +58,19 @@ def insert_one_document(body: CollectionInsert = Body(...)):
         logger.error(e)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
+@router.post("/insertMany", response_model=Document,
+             responses={200: {"model": Any}, 400: {"model": exception.HTTPError}})
+def insert_many_document(body: CollectionInsertMany = Body(...)):
+    try:
+        document_metadata = jsonable_encoder(body)
+        db = client[document_metadata['database']]
+        for i in range(len(document_metadata['document'])):
+            single_doc = db[document_metadata['collection']].insert_one(document_metadata['document'][i]).inserted_id
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(json_util.dumps(single_doc)))
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
 
 @router.post("/updateOne", response_model=Document,
              responses={200: {"model": Any}, 400: {"model": exception.HTTPError}})
@@ -62,6 +87,20 @@ def update_one_document(body: UpdateDocument = Body(...)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
 
+@router.post("/updateMany", response_model=Document,
+             responses={200: {"model": Any}, 400: {"model": exception.HTTPError}})
+def update_many_document(body: UpdateDocument = Body(...)):
+    try:
+        document_metadata = jsonable_encoder(body)
+        db = client[document_metadata['database']]
+        all_doc = db[document_metadata['collection']].update_many(filter=document_metadata['filter'],
+                                                                 update=document_metadata['update'])
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(json_util.dumps(all_doc.modified_count)))
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+
 @router.post("/deleteOne", response_model=Document,
              responses={200: {"model": Any}, 400: {"model": exception.HTTPError}})
 def delete_one_document(body: CollectionModel = Body(...)):
@@ -69,6 +108,18 @@ def delete_one_document(body: CollectionModel = Body(...)):
         document_metadata = jsonable_encoder(body)
         db = client[document_metadata['database']]
         single_doc = db[document_metadata['collection']].find_one_and_delete(filter=document_metadata['filter'])
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(json_util.dumps(single_doc)))
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+@router.post("/deleteMany", response_model=Document,
+             responses={200: {"model": Any}, 400: {"model": exception.HTTPError}})
+def delete_many_document(body: CollectionModel = Body(...)):
+    try:
+        document_metadata = jsonable_encoder(body)
+        db = client[document_metadata['database']]
+        single_doc = db[document_metadata['collection']].delete_many(filter=document_metadata['filter'])
         return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(json_util.dumps(single_doc)))
     except Exception as e:
         logger.error(e)
